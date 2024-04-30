a927c28b68d7d5136e81ef2f1b0cae561bf68c54
Kenneth Brattli <kenneth.brattli@tecnico.ulisboa.pt>
2024-04-30 13:56:50 +0100

    add selled product ms

---
+++ LaaS-2024/microservices/selledproduct/src/test/java/org/ie/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.ie;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
