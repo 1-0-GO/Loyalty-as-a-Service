29e14715b976088ea1ab9a7a6a764abf4e2d2302
Lucasveiga02 <lucas.veiga@mines-albi.fr>
2024-05-07 13:06:09 +0100

    fef

---
+++ Terraform/Quarkus-Terraform/purchase/quarkus.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Starting..."
+
+sudo yum install -y docker
+
+sudo service docker start
+
+sudo docker login -u "tweekzio" -p "Seiadmin123!"
+
+sudo docker pull tweekzio/customer:1.0.0-SNAPSHOT
+
+sudo docker run -d --name customer -p 8080:8080 tweekzio/customer:1.0.0-SNAPSHOT
+
+echo "Finished."
+
+
+
---
+++ Terraform/Quarkus-Terraform/shop/QuarkusShopInstall.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region      = "us-east-1"
+}
+
+resource "aws_instance" "shop" {
+  ami                     = "ami-0cf10cdf9fcd62d37"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = [aws_security_group.instance.id]
+  key_name                = "vockey"
+  user_data = "${file("quarkus.sh")}"
+  user_data_replace_on_change = true
+  
+  tags = {
+    Name = "ShopMS-Instance"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = var.security_group_name
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+variable "security_group_name" {
+  description = "The name of the security group"
+  type        = string
+  default     = "ShopMS-SecurityGroup"
+}
